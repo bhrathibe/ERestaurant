6af166ea5453dc19dfeadac7684e309950f27806
KUMAREPKM <kumar.epkm@gmail.com>
2023-09-30 15:07:01 +0530

    Add project files.

---
+++ Controllers/FoodItemController.cs
@@ -0,0 +1,77 @@
+﻿using ERestaurant.DataConnection;
+using ERestaurant.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ERestaurant.Controllers
+{
+    public class FoodItemController : Controller
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public FoodItemController(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var foodItems = _dbContext.FoodItemsTable.ToList();
+            return View(foodItems);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(FoodItem foodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.FoodItemsTable.Add(foodItem);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(foodItem);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var foodItem = _dbContext.FoodItemsTable.Find(id);
+            return View(foodItem);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(FoodItem foodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(foodItem).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(foodItem);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var foodItem = _dbContext.FoodItemsTable.Find(id);
+            return View(foodItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var foodItem = _dbContext.FoodItemsTable.Find(id);
+            _dbContext.FoodItemsTable.Remove(foodItem);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using ERestaurant.DataConnection;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRazorPages();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddDbContext<RestaurantDbContext>(options =>
+  //  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<RestaurantDbContext>(options =>
+    options.UseInMemoryDatabase(databaseName: "InMemoryDatabase"));
+
+
+var app = builder.Build();
+
+ 
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/FoodItem/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=FoodItem}/{action=Index}/{id?}");
+
+app.Run();
